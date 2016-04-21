fa8d8fce1db7ffc910a161785c094f04718d7cd5
winstonou <ouwinston@berkeley.edu>
2016-04-21 01:25:56 -0400

    initiate tuples

--- ionization_energies.py
+++ ionization_energies.py
@@ -4,7 +4,10 @@
 #kfine
 import numpy as np
 def function(element,charge):
-	#initialize arrays
+	#initialize tuples 
+	#element tuples
+	element = (null,null,He, Li ,Be ,B ,C ,N ,O ,F ,Ne ,Na ,Mg ,Al ,Si ,P ,S ,Cl ,Ar ,K ,Ca ,Sc ,Ti,V ,Cr,Mn,Fe ,Co ,Ni ,Cu ,Zn ,Ga ,Ge ,As ,Se ,Br ,Kr ,Rb ,Sr ,Y ,Zr ,Mo ,Tc ,Ru ,Rh ,Pd ,Ag ,Cd ,In ,Sn ,Sb ,Te ,I ,Xe ,Cs ,Ba ,La ,Ce ,Pr ,Nd ,Pm ,Sm ,Eu ,Gd ,Tb ,Dy ,Ho ,Er ,Tm ,Yb ,Lu ,Hf ,Ta ,W ,Re ,Os ,Ir ,Pt ,Au ,Hg ,Tl ,Pb ,Bi ,Po ,At ,Rn ,Fr ,Ra ,Ac ,Th ,Pa,U,Np,Pu,Am,Cm ,Bk,Cf,Es)
+	#individual tuples
 	He = [null]
 	Li = [null]
 	Be = [null]
